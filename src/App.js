bcb174757878d2cb182b8128ea8b78bc5af74cb4
cassiano-dio <cassiano@dio.me>
2022-09-23 11:53:30 -0300

    Commit 5

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Nova versão do site do Bruxo</h1>
-      </header>
       <body>
       <img src={"https://render.fineartamerica.com/images/rendered/default/flat/round-beach-towel/images/artworkimages/medium/3/1-ronaldinho-siva-nipa.jpg?&targetx=-2&targety=-33&imagewidth=788&imageheight=957&modelwidth=788&modelheight=788&backgroundcolor=035496&orientation=0"} className="App-logo" alt="Bruxaria" />
       </body>
